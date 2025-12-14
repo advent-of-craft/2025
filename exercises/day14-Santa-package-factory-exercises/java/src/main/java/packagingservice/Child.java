bb01dc77e9af96cb01e9b45a259ef6e8ab715180
Fabrice Bibonne <fabrice.bibonne@courriel.eco>
2025-12-14 18:43:34 +0100

    refactor (Use builder and record for domain model objects)

--- exercises/day14-Santa-package-factory-exercises/java/src/main/java/packagingservice/Child.java
+++ exercises/day14-Santa-package-factory-exercises/java/src/main/java/packagingservice/Child.java
@@ -1,39 +1,8 @@
 package packagingservice;
 
-public class Child {
+import io.soabase.recordbuilder.core.RecordBuilder;
 
-    private final String name;
-    private final int age;
-    private final ChildGender gender;
-    private final boolean hasBeenNice;
-    private final Gift assignedGift;
+@RecordBuilder
+public record Child(String name, int age, ChildGender gender, boolean hasBeenNice, Gift assignedGift) {
 
-    public Child(String name, int age, ChildGender gender,
-                 boolean hasBeenNice, Gift assignedGift) {
-        this.name = name;
-        this.age = age;
-        this.gender = gender;
-        this.hasBeenNice = hasBeenNice;
-        this.assignedGift = assignedGift;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public ChildGender getGender() {
-        return gender;
-    }
-
-    public boolean hasBeenNice() {
-        return hasBeenNice;
-    }
-
-    public Gift getAssignedGift() {
-        return assignedGift;
-    }
 }
--- exercises/day14-Santa-package-factory-exercises/java/src/main/java/packagingservice/Gift.java
+++ exercises/day14-Santa-package-factory-exercises/java/src/main/java/packagingservice/Gift.java
@@ -1,32 +1,8 @@
 package packagingservice;
 
-public class Gift {
+import io.soabase.recordbuilder.core.RecordBuilder;
 
-    private final String name;
-    private final GiftSize size;
-    private final boolean fragile;
-    private final int recommendedMinAge;
+@RecordBuilder
+public record Gift(String name, GiftSize size, boolean fragile, int recommendedMinAge) {
 
-    public Gift(String name, GiftSize size, boolean isFragile, int recommendedMinAge) {
-        this.name = name;
-        this.size = size;
-        this.fragile = isFragile;
-        this.recommendedMinAge = recommendedMinAge;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public GiftSize getSize() {
-        return size;
-    }
-
-    public boolean isFragile() {
-        return fragile;
-    }
-
-    public int getRecommendedMinAge() {
-        return recommendedMinAge;
-    }
 }
